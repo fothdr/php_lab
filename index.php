9ff7d41a86c86da5713cfa8cb7944e78f3c25e68
Nikitin <60608987+fothdr@users.noreply.github.com>
2021-12-05 17:08:10 +0500

    Создание  index.php и формирование структуры страницы

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Никитин А.</title>
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css" type="text/css" />
+
+</head>
+<body>
+<div class="info">
+<h2>Никитин Александр</h6>
+<p> Лабораторные работы, группа ПИ-322 </p>
+<a href="#" style="text-decoration: none; color: black;"><p> Ссылка на репозиторий(github) </p></a>
+</div>
+<div class="container">
+<div class="list-group">
+  <a href="lab1.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+    <img src="https://cdn-icons.flaticon.com/png/128/5948/premium/5948230.png?token=exp=1638700220~hmac=1ed556defaecda5d42f9b4c913cec08a" alt="twbs" width="32" height="32" >
+    <div class="d-flex gap-2 w-100 justify-content-between">
+      <div>
+        <h6 class="mb-0">Лабораторная работа №1</h6>
+        <p class="mb-0 opacity-75">По списку группы - 15 </p>
+      </div>
+      
+    </div>
+  </a>
+  <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+    <img src="https://cdn-icons.flaticon.com/png/128/5948/premium/5948235.png?token=exp=1638700220~hmac=2482f8fce5c4a891a286ad45c483a9d3" alt="twbs" width="32" height="32" >
+    <div class="d-flex gap-2 w-100 justify-content-between">
+      <div>
+        <h6 class="mb-0">Лабораторная работа №2</h6>
+        <p class="mb-0 opacity-75">Некоторый заполнитель в абзаце</p>
+      </div>
+    </div>
+  </a>
+</div>
+</div>
+</body>
+</html>
