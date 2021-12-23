4fda29f8f8c4d1299958b88133edd59cf5d6f550
Nikitin <60608987+fothdr@users.noreply.github.com>
2021-12-24 03:58:30 +0500

    Создание файла lab2-6.php и выполнение задания 6 с вызовом функций

---
+++ lab2-6.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Никитин А - ПИ-322</title>
+    
+</head>
+<body>
+    <div style="max-width: 600px; margin: 0 auto;">
+    <h2>ЗАДАЧА №2-6 Вариант 15 и 4</h2>
+    <h3> Вариант 4 </h3>
+    <? include 'nikitin.php'; ?>
+    <?php 
+     getTask4();
+     createMas4();
+     solveMas4();
+    ?>
+    <h3> Вариант 15 </h3>
+    <?php 
+     getTask15();
+     createMas15();
+     solveMas15();
+    ?>
+
+    
+</div>
+</body>
+</html>
