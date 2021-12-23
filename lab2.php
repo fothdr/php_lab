e46b3ec9e3c9f9fca8b1cfce17141a572533037e
Nikitin <60608987+fothdr@users.noreply.github.com>
2021-12-24 03:55:40 +0500

    Создание lab2.php с ссылками на задания

---
+++ lab2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Никитин А - ПИ-322</title>
+    
+</head>
+<body>
+    <div class="info" style="max-width: 600px; margin: 0 auto;">
+    <h2>САМОСТОЯТЕЛЬНАЯ РАБОТА</h2>
+    <a class="task-link" href="lab2-1.php">ЗАДАЧА №2-1</a><br>
+    <a class="task-link" href="lab2-2.php">ЗАДАЧА №2-2</a><br>
+    <a class="task-link" href="lab2-3.php">ЗАДАЧА №2-3</a><br>
+    <a class="task-link" href="lab2-4.php">ЗАДАЧА №2-4</a><br>
+    <a class="task-link" href="lab2-5.php">ЗАДАЧА №2-5</a><br>
+    <a class="task-link" href="lab2-6.php">ЗАДАЧА №2-6</a><br>
+   
+    </div>
+    <style>.task-link {
+        display: block; max-width: 150px; color: #000; font-size: 20px; margin-bottom: 6px;
+    } </style>
+</body>
+</html>
