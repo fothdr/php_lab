d4647350c55c71710c74551b2b9750f9c9495ca4
Nikitin <60608987+fothdr@users.noreply.github.com>
2021-12-24 23:40:47 +0500

    Добавление ссылок на задачи 3 лабораторной

---
+++ lab3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Никитин А - ПИ-322</title>
+    
+</head>
+<body>
+    <div class="info" style="max-width: 600px; margin: 0 auto;">
+    <h2>САМОСТОЯТЕЛЬНАЯ РАБОТА</h2>
+    <a class="task-link" href="s3-1.html">ЗАДАЧА №3-1</a><br>
+    <a class="task-link" href="s3-2.php">ЗАДАЧА №3-2</a><br>
+    <a class="task-link" href="s3-3.php">ЗАДАЧА №3-3</a><br>
+    <a class="task-link" href="s3-4.php">ЗАДАЧА №3-4</a><br>
+    <a class="task-link" href="s3-5.php">ЗАДАЧА №3-5</a><br>
+    <a class="task-link" href="s3-6.html">ЗАДАЧА №3-6</a><br>
+   
+    </div>
+    <style>.task-link {
+        display: block; max-width: 150px; color: #000; font-size: 20px; margin-bottom: 6px;
+    } </style>
+</body>
+</html>
