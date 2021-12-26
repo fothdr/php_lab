836361d469a611100842ba4cd5834faa7b68d8da
Nikitin <60608987+fothdr@users.noreply.github.com>
2021-12-26 15:12:37 +0500

    Добавление ссылки на ЛР4

--- index.php
+++ index.php
@@ -46,6 +46,15 @@
       </div>
     </div>
   </a>
+      <a href="bdgames/" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+    <img src="img/number-4.png" alt="twbs" width="32" height="32" >
+    <div class="d-flex gap-2 w-100 justify-content-between">
+      <div>
+        <h6 class="mb-0">Лабораторная работа №4</h6>
+        <p class="mb-0 opacity-75">*выполнена*</p>
+      </div>
+    </div>
+  </a>
 </div>
 </div>
 </body>
