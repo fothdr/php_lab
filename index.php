456315dc3c5e3efc12872c357e66c0d668f372cf
Nikitin <60608987+fothdr@users.noreply.github.com>
2021-12-26 23:27:07 +0500

    Обновление для 5 ЛР

--- index.php
+++ index.php
@@ -1,61 +1,128 @@
 <!DOCTYPE html>
+
 <html lang="ru">
+
 <head>
+
   <meta charset="UTF-8">
+
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title>Никитин А.</title>
+
   
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
   <link rel="stylesheet" href="css/style.css" type="text/css" />
 
+
+
 </head>
+
 <body>
+
 <div class="info">
+
 <h2>Никитин Александр</h6>
+
 <p> Лабораторные работы, группа ПИ-322 </p>
-<a href="#" style="text-decoration: none; color: black;"><p> Ссылка на репозиторий(github) </p></a>
+
+<a href="https://github.com/fothdr/php_lab" style="text-decoration: none; color: black;" target="_blank"><p> Ссылка на репозиторий(github) </p></a>
+
 </div>
+
 <div class="container">
+
 <div class="list-group">
-  <a href="lab1.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
-   <img src="img/number-1.png" alt="twbs" width="32" height="32" >
+
+  <a href="lab1.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true" target="_blank">
+
+    <img src="img/number-1.png" alt="twbs" width="32" height="32" >
+
     <div class="d-flex gap-2 w-100 justify-content-between">
+
       <div>
+
         <h6 class="mb-0">Лабораторная работа №1</h6>
-        <p class="mb-0 opacity-75">По списку группы - 15 </p>
+
+        <p class="mb-0 opacity-75">*выполнена и принята* </p>
+
       </div>
+
       
+
     </div>
+
   </a>
-  <a href="lab2.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
-   <img src="img/number-2.png" alt="twbs" width="32" height="32" >
+
+  <a href="lab2.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true" target="_blank">
+
+    <img src="img/number-2.png" alt="twbs" width="32" height="32" >
+
     <div class="d-flex gap-2 w-100 justify-content-between">
+
       <div>
+
         <h6 class="mb-0">Лабораторная работа №2</h6>
-        <p class="mb-0 opacity-75">*выполнена*</p>
+
+        <p class="mb-0 opacity-75">*выполнена и принята*</p>
+
       </div>
+
     </div>
+
   </a>
+
     <a href="lab3.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+
     <img src="img/number-3.png" alt="twbs" width="32" height="32" >
+
     <div class="d-flex gap-2 w-100 justify-content-between">
+
       <div>
+
         <h6 class="mb-0">Лабораторная работа №3</h6>
-        <p class="mb-0 opacity-75">*в процессе*</p>
+
+        <p class="mb-0 opacity-75">*выполнена и принята*</p>
+
       </div>
+
     </div>
+
   </a>
-      <a href="bdgames/" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
-    <img src="img/number-4.png" alt="twbs" width="32" height="32" >
-    <div class="d-flex gap-2 w-100 justify-content-between">
-      <div>
-        <h6 class="mb-0">Лабораторная работа №4</h6>
-        <p class="mb-0 opacity-75">*выполнена*</p>
-      </div>
-    </div>
+
   </a>
+
+</a>
+
+<a href="bd_games/auth.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+
+<img src="img/number-4.png" alt="twbs" width="32" height="32" >
+
+<div class="d-flex gap-2 w-100 justify-content-between">
+
+  <div>
+
+    <h6 class="mb-0">Лабораторная работа №4 и №5</h6>
+
+    <p class="mb-0 opacity-75">*выполнена* (данные от аккаунта на почте)</p>
+
+  </div>
+
 </div>
+
+</a>
+
+</a>
+
+
 </div>
+
+</div>
+
 </body>
+
 </html>
